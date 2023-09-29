984513c7792242f8b4851c5482e0ecdd1d227831
joser <joser@MENDOZA>
2023-09-28 18:44:47 -0600

    practica de jueves 28 de septiembre de 2023

--- Module1.vb
+++ Module1.vb
@@ -1,6 +1,14 @@
-﻿Module Module1
+﻿Imports System.Net
+
+Module Module1
 
     Sub Main()
+
+        'PracticaMartes26sep2023()
+        PracticaJueves28sep2023()
+        Console.Read()
+    End Sub
+    Sub PracticaMartes26sep2023()
         FunCType()
         FunCBool()
         FunCByte()
@@ -8,7 +16,7 @@
         FunCInt()
         FunCStr()
         FunCDatate()
-        Console.Read()
+
     End Sub
 
     Sub FunCType() 'conversio de dato a tipo Single
@@ -93,6 +101,108 @@
 
         hora = CDate(horaStr)
         Console.WriteLine(hora)
+
+    End Sub
+
+    Sub PracticaJueves28sep2023()
+        'Operadores()
+        'FonWhile()
+        'DoWhile()
+        cicloFor()
+    End Sub
+
+    Sub Operadores()
+
+        Dim a, b As Integer
+        a = 12
+        b = 5
+        Console.WriteLine("Los valores de la variables a operar  son: a = " & a & " y b= " & b)
+
+        'a = a + b
+        ' a += b
+        ' Console.WriteLine("el resultado de la suma es " & a)
+
+        'a = a - b
+        ' a -= b
+        ' Console.WriteLine("el resultado de la resta es " & a)
+
+        'a = a * b
+        'a *= b
+        'Console.WriteLine("el resultado de la multiplicacion es " & a)
+
+        'a = a / b
+        ' a /= b
+        'Console.WriteLine("el resultado de la divicion es " & a)
+
+        'a = a + b
+        a = a Mod b
+        Console.WriteLine("el resultado del resto es " & a)
+    End Sub
+
+    Sub FonWhile()
+        Console.WriteLine("por favor ingrese un numero: ")
+        Dim i = 1
+        While i <= 10
+            Console.WriteLine("El interador = " & i)
+            i += 1
+            'es como que yo diga i = i+1. Es lo mismo i += 1
+        End While
+        Console.WriteLine(i) 'nuevo valor de i
+
+        While i >= 1
+            Console.WriteLine("El interador = " & i)
+            i -= 1
+            'es como que yo diga i = i+1. Es lo mismo i -= 1
+        End While
+
+
+    End Sub
+
+    Sub DoWhile()
+        Dim i = 1
+        'Do
+        '    Console.WriteLine("El iterador = " & i)
+        '    i += 4
+
+        'Loop While i <= 10
+
+
+        Dim random = New Random()
+        Dim aleatorio = random.Next(1, 6)
+        Console.WriteLine("El valor random es: " & aleatorio)
+        Do
+            Console.WriteLine("El iterador = " & i)
+            i += 1
+
+        Loop While i <= aleatorio
+
+    End Sub
+
+    Sub cicloFor()
+
+        'For i = 1 To 10
+        '    Console.WriteLine("El itterador = " & i)
+
+        'Next
+
+
+        'For i = 1 To 10 Step 1
+        '    Console.WriteLine("El itterador = " & i)
+
+        'Next
+
+        'For i = 1 To 10 Step -1
+        '    Console.WriteLine("El itterador = " & i)
+
+        'Next
+
+        Dim a, b As Integer
+        a = 10
+        b = Integer.Parse(Console.ReadLine())
+
+        For i = a To b Step -1
+            Console.WriteLine(" El iterador es = " & i)
+        Next
 
     End Sub
 
